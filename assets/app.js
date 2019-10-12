bfe25c00d933231dd2f817ba70c9e231c201595b
Greatest Dane <DaneGoodman85@gmail.com>
2019-10-12 15:23:46 -0700

    I just copied and pasted the CSS from the github repo to sanddbox

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,12 @@
 
 window.addEventListener( "keydown", (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(e.keyCode);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+
+    key.classList.add("playing");
 
 });
 
