fca5217bdc47297b3e72dfd5b8019eef64b6212b
Greatest Dane <DaneGoodman85@gmail.com>
2019-10-14 19:29:39 -0700

    I have all the finishing touches done, anything past this is just extra flair

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,26 @@
 
-window.addEventListener( "keydown", (e) => {
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(e.keyCode);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // console.log(e.keyCode);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-
     key.classList.add("playing");
+};
 
-});
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return; // Skip it if it's not a transform
+    // console.log(e.propertyName);
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach( key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener( "keydown", playSound);
+
+
+
 
